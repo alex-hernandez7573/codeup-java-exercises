2ea03edd5e67007bb52a1e48a041b5e3b90ae746
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-05-24 14:14:55 -0500

    introduction to java ex and lecture

---
+++ src/HelloWorld.java
@@ -0,0 +1,137 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+//                System.out.print("Hello");
+//                System.out.print(", World!");
+//                System.out.println("\nDoge 1 Doge 2 Doge 3");
+//
+//                String name = "\nAlex";
+//                System.out.println(name);
+//
+//                int thisNum = 15;
+//                System.out.println(thisNum);
+//
+//                int crazyNum = 15;
+//                crazyNum = 25;
+//                System.out.println(crazyNum);
+//
+//                float thisFloat = 5.99f;
+//                System.out.println(thisFloat+" you can concat?");
+//
+//                char myLetter = 'A';
+//                System.out.println(myLetter+" :: CHAR");
+//
+//                boolean testBool = true;
+//                System.out.println(testBool);
+//
+//                String myText = "lalalala hello world?";
+//                System.out.println(myText);
+//
+//                String firstName = "Alexander";
+//                String lastName = " Hernandez";
+//                String fullName = firstName + lastName;
+//                System.out.println(fullName);
+//
+//                int x = 5;
+//                int y = 5;
+//                System.out.println(x+y);
+//
+//                int a = 5;
+//                int b = 10;
+//                int c = 15;
+//                System.out.println(a+b+c+" addition");
+//                System.out.println(c/a+" division");
+//                System.out.println(b-a+" Subtraction");
+//                System.out.println(b*a+" Multiplication");
+//
+//
+//
+//                int minsPerHour = 60;
+//
+//                // data type that can store whole number from -128 to 127
+//
+//                byte thisByte = 100;
+//                System.out.println(thisByte);
+//
+//                // data type that can store whole numbers -2147483648 to 2147483648
+//                int testNumber = 100000;
+//                System.out.println(testNumber);
+//
+//
+//                ///increment
+//        int d = 5;
+////        int incremTheD = d++;
+
+        // TO DO
+        // Create an int variable named myFavoriteNumber
+        // and assign your favorite number to it, then print it out to the console.
+
+
+        int myFavoriteNumber = 25;
+        System.out.println(myFavoriteNumber + " this is my favorite number");
+
+        //Create a String variable named myString
+        // and assign a string value to it, then print the variable out to the console.
+
+        String myString = "Castle";
+        System.out.println(myString);
+
+        //Change your code to assign a character value to myString. What do you notice?
+        myString = "glass";
+        System.out.println(myString);
+
+        //Change your code to assign the value 3.14159 to myString. What happens?
+
+//        myString = 3.14159; <-- java: incompatible types: double cannot be converted to java.lang.String
+
+        //Declare an long variable named myNumber, but do not assign anything to it.
+        // Next try to print out myNumber to the console. What happens?
+//        long myNumber;
+//        System.out.println(myNumber);
+        //java: variable myNumber might not have been initialized
+
+       // Change your code to assign the value 3.14 to myNumber. What do you notice?
+
+//        long myNumber =3.14;
+
+        ///java: incompatible types: possible lossy conversion from double to long
+
+//        Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+        long myNumber = 123L;
+        System.out.println(myNumber);
+        // prints 123 without the L
+
+
+        /// Change your code to assign the value 123 to myNumber.
+
+        myNumber = 123;
+        System.out.println(myNumber);
+        // prints 123
+
+        ///Change your code to declare myNumber as a float. Assign the value 3.14 to it.
+        // What happens? What are two ways we could fix this?
+
+//        myNumber = 3.14F;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x); // one is incrementing the other doesnt
+
+        //Try to create a variable named class. What happens?
+
+        class MyClass {
+            int alex =12;
+
+            public int getAlex() {
+                return  alex;
+            }
+        }
+
+
+
+    }
+
+}
